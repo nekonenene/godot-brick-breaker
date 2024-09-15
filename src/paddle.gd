b5dd34e1d93cd762d25bed739df3888395f6da8f
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2024-09-15 21:58:33 +0900

    Paddle が Wall に重ならないようにした

--- src/paddle.gd
+++ src/paddle.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 var initial_position: Vector2
 var dragging: bool
-var move_tween: Tween
+var target_x: float = -1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,31 +10,28 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	var collided = move_and_slide()
-	if collided:
-		print("Wow, collided with ", get_last_slide_collision().get_collider().name)
-
-		# if move_tween.is_running():
-		# 	move_tween.stop()
-	#pass
+	pass
 
 func _input(event):
-	# 0.1 秒かけて、入力された X 座標に移動する
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
-			print("mouse pressed at ", event.position)
+			#print("mouse pressed at ", event.position)
 			dragging = true
-			move(event.position.x)
+			target_x = event.position.x
 		else:
-			print("mouse released at ", event.position)
+			#print("mouse released at ", event.position)
 			dragging = false
 	elif event is InputEventMouseMotion and dragging:
-		print("mouse dragged to ", event.position)
-		move(event.position.x)
+		#print("mouse dragged to ", event.position)
+		target_x = event.position.x
 
-# 入力された X 座標に移動する
-func move(x: float) -> void:
-	var sec = 0.2
-
-	move_tween = get_tree().create_tween()
-	move_tween.tween_property(self, "position", Vector2(x, initial_position.y), sec)
+func _physics_process(_delta: float) -> void:
+	if target_x != -1 and position.x != target_x:
+		var diff = target_x - position.x
+		#print("diff: ", diff)
+		if abs(diff) > 1:
+			var speed = 6
+			var distance = log(abs(diff)) * speed
+			if distance > abs(diff):
+				distance = abs(diff)
+			move_and_collide(Vector2(distance * (diff / abs(diff)), 0))
