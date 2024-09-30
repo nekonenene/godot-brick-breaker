984a6f699f59ed2ec0ed5b7b5a42437c4cca3a92
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2024-10-01 03:05:26 +0900

    角度がいい感じになるような調整をするように

--- src/ball.gd
+++ src/ball.gd
@@ -4,12 +4,14 @@ signal wall_hit
 signal paddle_hit
 signal block_hit
 
+var initial_speed = 600
 var speed
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	reset_speed()
 	velocity = Vector2(randf_range(-1, 1), randf_range(-1, 1))
+	adjust_velocity()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -17,22 +19,24 @@ func _process(delta: float) -> void:
 
 	if collision:
 		var collider = collision.get_collider()
-		print("I collided with ", collider.name)
+		#print("I collided with ", collider.name)
 
 		if collider.name == "Wall":
 			# 壁に当たったら反射
 			velocity = velocity.bounce(collision.get_normal())
-			reset_speed()
+			adjust_velocity()
 			wall_hit.emit()
 		elif collider.name == "Paddle":
+			print("spped:", speed)
 			# パドルに当たったら反射
 			velocity = velocity.bounce(collision.get_normal())
+			adjust_velocity()
 			reset_speed()
 			paddle_hit.emit()
 		elif collider.is_in_group("Blocks"):
 			# ブロックに当たったら反射
 			velocity = velocity.bounce(collision.get_normal())
-			reset_speed()
+			adjust_velocity()
 			block_hit.emit()
 			collider.queue_free()
 
@@ -45,4 +49,18 @@ func _process(delta: float) -> void:
 		reset_speed()
 
 func reset_speed() -> void:
-	speed = randi_range(400, 800)
+	speed = initial_speed
+
+# 進む角度が真横や真上にならないように調整する
+func adjust_velocity() -> void:
+	var degree = rad_to_deg(velocity.angle())
+	if (-10 < degree && degree <= 0) || (170 < degree && degree <= 180):
+		var rotate_degree = randf_range(-10, -5)
+		velocity = velocity.rotated(deg_to_rad(rotate_degree))
+		print("adusted degree:", rad_to_deg(velocity.angle()))
+	elif (-180 <= degree && degree < -170) || (0 <= degree && degree < 10):
+		var rotate_degree = randf_range(5, 10)
+		velocity = velocity.rotated(deg_to_rad(rotate_degree))
+		print("adusted degree:", rad_to_deg(velocity.angle()))
+	elif abs(degree) == 90:
+		velocity = velocity.rotated(deg_to_rad(0.1))
