da3048dfc7814f51cf7f0de4c022d0ec7e2f269b
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2024-10-02 20:57:27 +0900

    タイトル画面からフェードしつつゲーム画面に進むように

---
+++ src/start_view.gd
@@ -0,0 +1,37 @@
+extends Control
+
+var main_scene = preload("res://main.tscn").instantiate()
+var is_started = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$AudioStreamPlayer.play()
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.is_action_pressed("press_accept_key"):
+			start_process()
+
+func _on_start_button_pressed() -> void:
+	start_process()
+
+func start_process() -> void:
+	if is_started:
+		return
+
+	print("Start button pressed")
+	is_started = true
+
+	$TransitionRect.visible = true
+	var transition_sec = 0.5
+	var tween = get_tree().create_tween()
+	# トランジション用の黒い四角のアルファ値を徐々に上げる
+	tween.tween_property($TransitionRect, "color:a", 1, transition_sec)
+	# 同時に音楽をフェードアウト
+	tween.parallel().tween_property($AudioStreamPlayer, "volume_db", -80, transition_sec)
+	await tween.finished
+
+	# メインシーンを追加
+	get_tree().root.add_child(main_scene)
+	# 現在のシーンをメモリから解放
+	queue_free()
