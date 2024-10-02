5494706bf6309c4e1f387c4f554d6d9037d7e2bb
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2024-10-03 04:34:47 +0900

    Export for v0.1.0

--- builds/html/index.service.worker.js
+++ builds/html/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1727883294|37008801455';
+const CACHE_VERSION = '1727897666|13074690678';
 /** @type {string} */
 const CACHE_PREFIX = 'Brick Breaker Sa-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
